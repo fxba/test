5aee5d8bf66b205d75bf45e84331a6e63834a2e7
Franz Xaver Bachler <franzxaverbachler@pop-os.localdomain>
2020-08-31 16:30:24 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+lastname = "lovelace"
+firstname = "Lada"
+
+fullname = firstname + " " + lastname
+
+message = "hello, " + fullname.title() + "!"
+print(message)
+
+print("\nPhyton")
+
+
+
